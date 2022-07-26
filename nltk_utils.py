5331ebb58db87e8b7a288b164eeeb419f71582c6
Ahmad Williamson <ahmadwilliamson@The-Hidden-Genius-Project-70.local>
2022-07-26 14:19:55 -0700

    first commit

--- nltk_utils.py
+++ nltk_utils.py
@@ -17,11 +17,11 @@ def bag_of_words(tokenized_sentence, all_words):
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool"] 
     bog = [  0  ,     1   ,  0,    1 ,    0 ,    0  ,     0]
     """
-    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    tokenized_sentence = [stem(word) for word in tokenized_sentence]
    
     bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
         if w in tokenized_sentence:
             bag[idx] = 1.0
  
-        return bag
+    return bag
--- train.py
+++ train.py
@@ -23,10 +23,14 @@ for intent in intents['intents']:
         all_words.extend(w)
         xy.append((w, tag))
 
-ignore_words = ['?', '!', '.', ',']
+ignore_words = ['?', '!', '.']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
+
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique stemmed words:", all_words)
 
 X_train = []
 y_train = []
@@ -40,6 +44,14 @@ for (pattern_sentence, tag) in xy:
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
+# Hyperparameters
+batchsize = 8
+hidden_size = 8 
+output_size = len(tags)
+input_size = len(X_train[0])
+learning_rate = 0.001
+num_epochs = 1000
+print(input_size, output_size)
 class ChatDataset(Dataset):
     def __init__(self):
         self.n_samples = len(X_train)
@@ -53,17 +65,11 @@ class ChatDataset(Dataset):
     def __len__(self):
         return self.n_samples
 
-# Hyperparameters
-batchsize = 8
-hidden_size = 8 
-output_size = len(tags)
-input_size = len(X_train[0])
-learning_rate = 000.1
-num_epochs = 1000
+
 
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batchsize, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batchsize, shuffle=True, num_workers=0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNet(input_size, hidden_size, output_size).to(device) 
@@ -75,7 +81,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
         words = words.to(device)
-        labels = labels.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
 
         # forward
         outputs = model(words)
@@ -90,3 +96,17 @@ for epoch in range(num_epochs):
         print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
     
 print(f'final loss, loss={loss.item():.4f}')    
+
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "all_words": all_words,
+    "tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
